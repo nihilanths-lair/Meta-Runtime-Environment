3df918e7df3fb5151c80a3a22cadf2119b0fb240
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-08-19 06:00:54 +1100

    Update code-editor.c

--- tools/code-editor.c
+++ tools/code-editor.c
@@ -7,8 +7,15 @@ void CodeEditor();
 int main(int argc, char *argv[])
 {
     setlocale(0, "");
-    printf(" Количество аргументов: %u", argc);
-    for (int i = 0; i < argc; i++) printf("\n Аргумент %u: %s", i+1, argv[i]);
+    printf(" Количество аргументов: %u\n", argc);
+    for (int i = 0; i < argc; i++) printf(" Аргумент %u: %s\n", i+1, argv[i]);
+    if (argc == 2)
+    {
+        FILE *file = fopen(argv[1], "rb");
+        if (!file) return 0;
+        printf(" Файл: %s открыт на чтение", argv[1]);
+        fclose(file);
+    }
     CodeEditor();
     _getch();
     return 0;
