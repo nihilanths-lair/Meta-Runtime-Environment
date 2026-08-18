52fee4bf64c5f08434abad7445884ca6f2d31d1a
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-08-19 05:42:57 +1100

    Update code-editor.c

--- tools/code-editor.c
+++ tools/code-editor.c
@@ -3,8 +3,13 @@ void CodeEditor()
     return;
 }
 
+#include <stdio.h>
+#include <locale.h>
 int main(int argc, char *argv[])
 {
+    setlocale(0, "");
+    printf(" Количество аргументов: %u", argc);
+    for (int i = 0; i < argc; i++) printf("\n Аргумент %u: %s", i+1, argv[i]);
     CodeEditor();
     return 0;
 }
