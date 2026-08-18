dae9922a4d19b61eaf71963353ae677bcec45d8d
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-08-19 06:47:48 +1100

    Update code-editor.c

--- tools/code-editor.c
+++ tools/code-editor.c
@@ -24,9 +24,17 @@ int main(int argc, char *argv[])
         char *file_text = malloc(file_size);
         printf("\n %s", file_text);
         printf("\n ");
-        for (long i = 0; i <= 15/*file_size*/; i++) printf("%c", file_text[i]);
-        printf("\n");
-        for (long i = 0; i <= 15/*file_size*/; i++) printf(" %02X", file_text[i]&0xFF);
+        printf("Offset(h) |");
+        for (unsigned char i = 0; i <= 15; i++) printf(" %02X", i);
+        printf(" | ");
+        printf("Отображение в файле\n");
+        for (long j = 0; j < 2; j++)
+        {
+            printf("\n  %08X |", 0);
+            for (long i = 0; i <= 15/*file_size*/; i++) printf(" %02X", file_text[i]&0xFF);
+            printf(" | ");
+            for (long i = 0; i <= 15/*file_size*/; i++) printf("%c", file_text[i]);
+        }
         free(file_text); // делаем сразу, чтобы потом не забыть ..
         fclose(file);
     }
